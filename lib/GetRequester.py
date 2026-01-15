a9fcd9132f163a6023cbbb349c6d500274400637
bradley <fazebrad13@gmail.com>
2026-01-15 09:28:23 +0300

    finished lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,20 @@
-import requests
+# lib/GetRequester.py
+
+from urllib.request import urlopen
 import json
+import ssl
+
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        context = ssl._create_unverified_context()
+
+        with urlopen(self.url, context=context) as response:
+            return response.read()  
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body.decode("utf-8"))
